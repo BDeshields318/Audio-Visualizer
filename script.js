12fe44e6415b409dbc80be309bd3e8103ab35b4b
BDeshields318 <85285329+BDeshields318@users.noreply.github.com>
2023-01-15 20:05:38 -0800

    Added responsive background

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
 var song;
+var img;
 var fft;
 var particles = [];
 
 function preload() {
   song = loadSound("Endors Toi.mp3");
+  img = loadImage("tame2.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  fft = new p5.FFT();
+  imageMode(CENTER);
+  rectMode(CENTER);
+  fft = new p5.FFT(0.3);
+
+  img.filter(BLUR, 10);
 }
 
 function draw() {
   background(0);
-  stroke(255);
-  noFill();
 
   translate(width / 2, height / 2);
 
+  fft.analyze();
+  amp = fft.getEnergy(20, 200);
+
+  push();
+  if (amp > 220) {
+    rotate(random(-0.5, 0.5));
+  }
+
+  image(img, 0, 0, width + 100, height + 150);
+  pop();
+
+  var alpha = map(amp, 0, 255, 180, 150);
+  fill(0, alpha);
+  noStroke();
+  rect(0, 0, width, height);
+
+  stroke(255);
+  strokeWeight(5);
+  noFill();
+
   var wave = fft.waveform();
 
-  // for (var t = -1; t <= 1; t += 2) {
-  //   beginShape();
-  //   for (var i = 0; i <= 180; i++) {
-  //     var index = floor(map(i, 0, 180, 0, wave.length - 1));
+  for (var t = -1; t <= 1; t += 2) {
+    beginShape();
+    for (var i = 0; i <= 180; i++) {
+      var index = floor(map(i, 0, 180, 0, wave.length - 1));
 
-  //     var r = map(wave[index], -1, 1, 150, 350);
+      var r = map(wave[index], -1, 1, 150, 350);
 
-  //     var x = r * sin(i) * t;
-  //     var y = r * cos(i);
-  //     vertex(x, y);
-  //   }
-  //   endShape();
-  // }
+      var x = r * sin(i) * t;
+      var y = r * cos(i);
+      vertex(x, y);
+    }
+    endShape();
+  }
 
   var p = new Particle();
   particles.push(p);
 
-  for (var i = 0; i < particles.length; i++) {
-    particles[i].show();
+  for (var i = particles.length - 1; i >= 0; i--) {
+    if (!particles[i].edges()) {
+      particles[i].update(amp > 220);
+      particles[i].show();
+    } else {
+      particles.splice(i, 1);
+    }
   }
 }
 
@@ -60,10 +89,36 @@ class Particle {
     this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
 
     this.w = random(3, 5);
+
+    this.color = [random(200, 255), random(200, 255), random(200, 255)];
   }
+
+  update(cond) {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    if (cond) {
+      this.pos.add(this.vel);
+      this.pos.add(this.vel);
+      this.pos.add(this.vel);
+    }
+  }
+
+  edges() {
+    if (
+      this.pos.x < -width / 2 ||
+      this.pos.x > width / 2 ||
+      this.pos.y < -height / 2 ||
+      this.pos.y > height / 2
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   show() {
     noStroke();
-    fill(255);
+    fill(this.color);
     ellipse(this.pos.x, this.pos.y, this.w);
   }
 }
